98f66c9d9a5b9e1f1fbebe1e82e85334de76057c
snapbuy <snapbuy@naver.com>
2022-07-14 17:02:04 +0900

    cargo first run

---
+++ ch1/ch1-penguins/target/debug/ch1-penguins.d
@@ -0,0 +1,1 @@
+/Volumes/T7/git/rust-in-action/ch1/ch1-penguins/target/debug/ch1-penguins: /Volumes/T7/git/rust-in-action/ch1/ch1-penguins/src/main.rs
---
+++ ch1/ch1-penguins/target/debug/deps/ch1_penguins-f0bae137e40ea41b.d
@@ -0,0 +1,5 @@
+/Volumes/T7/git/rust-in-action/ch1/ch1-penguins/target/debug/deps/ch1_penguins-f0bae137e40ea41b: src/main.rs
+
+/Volumes/T7/git/rust-in-action/ch1/ch1-penguins/target/debug/deps/ch1_penguins-f0bae137e40ea41b.d: src/main.rs
+
+src/main.rs:
