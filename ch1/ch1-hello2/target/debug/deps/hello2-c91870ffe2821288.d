7c9ff913264a750b8aaf1c8fae6a4f2b88103b23
snapbuy <snapbuy@naver.com>
2022-08-01 21:57:36 +0900

    cargo run

---
+++ ch1/ch1-hello2/target/debug/deps/hello2-c91870ffe2821288.d
@@ -0,0 +1,5 @@
+/Volumes/T7/git/rust-in-action/ch1/ch1-hello2/target/debug/deps/hello2-c91870ffe2821288: src/main.rs
+
+/Volumes/T7/git/rust-in-action/ch1/ch1-hello2/target/debug/deps/hello2-c91870ffe2821288.d: src/main.rs
+
+src/main.rs:
---
+++ ch1/ch1-hello2/target/debug/hello2.d
@@ -0,0 +1,1 @@
+/Volumes/T7/git/rust-in-action/ch1/ch1-hello2/target/debug/hello2: /Volumes/T7/git/rust-in-action/ch1/ch1-hello2/src/main.rs
