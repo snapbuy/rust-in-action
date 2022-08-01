94d33086f1d46d0d9a232cb05f99f9915bada851
snapbuy <snapbuy@naver.com>
2022-08-01 22:06:08 +0900

    cargo run --release

---
+++ ch1/ch1-penguins/target/release/ch1-penguins.d
@@ -0,0 +1,1 @@
+/Volumes/T7/git/rust-in-action/ch1/ch1-penguins/target/release/ch1-penguins: /Volumes/T7/git/rust-in-action/ch1/ch1-penguins/src/main.rs
---
+++ ch1/ch1-penguins/target/release/deps/ch1_penguins-5f88e667a856f70c.d
@@ -0,0 +1,5 @@
+/Volumes/T7/git/rust-in-action/ch1/ch1-penguins/target/release/deps/ch1_penguins-5f88e667a856f70c: src/main.rs
+
+/Volumes/T7/git/rust-in-action/ch1/ch1-penguins/target/release/deps/ch1_penguins-5f88e667a856f70c.d: src/main.rs
+
+src/main.rs:
