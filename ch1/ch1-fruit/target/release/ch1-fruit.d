53a1ad2fc02eb2bccefb5cb5ff7650624a3fa004
snapbuy <snapbuy@naver.com>
2022-08-03 11:21:28 +0900

    new file:   ch1/ch1-fruit/target/CACHEDIR.TAG

---
+++ ch1/ch1-fruit/target/release/ch1-fruit.d
@@ -0,0 +1,1 @@
+/Volumes/T7/git/rust-in-action/ch1/ch1-fruit/target/release/ch1-fruit: /Volumes/T7/git/rust-in-action/ch1/ch1-fruit/src/main.rs
---
+++ ch1/ch1-fruit/target/release/deps/ch1_fruit-4722391eba743629.d
@@ -0,0 +1,5 @@
+/Volumes/T7/git/rust-in-action/ch1/ch1-fruit/target/release/deps/ch1_fruit-4722391eba743629: src/main.rs
+
+/Volumes/T7/git/rust-in-action/ch1/ch1-fruit/target/release/deps/ch1_fruit-4722391eba743629.d: src/main.rs
+
+src/main.rs:
